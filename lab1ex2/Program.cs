3fa8de883d178de97dfa8bb1f4d87dc4568a39d2
viorel.stan <viorel.stan@MP150173>
2024-02-17 09:22:57 +0200

    Add project files.

---
+++ lab1ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Scrieti un program care va calcula media aritmetica 
+ * a trei numere citite de la tastatura
+*/
+
+Console.WriteLine("Introduceti valoarea pentru primului numar");
+int numarA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduceti valoarea pentru al doilea numar");
+int numarB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduceti valoarea pentru al treilea numar");
+int numarC = int.Parse(Console.ReadLine());
+
+int mediaAritmetica = (numarA + numarB + numarC) / 3;
+
+Console.WriteLine("Media aritmetica a celor trei numere introduse este:" + mediaAritmetica);
